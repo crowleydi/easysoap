0921be4d8155cb698e2c2e53b205e6da92992caa
dcrowley <>
2001-08-22 18:53:39 +0000

    Added SOAPHex and setup to act as a wrapper around SOAPArrays so encoding style can be specified.

--- easysoap/include/SOAPBase64.h
+++ easysoap/include/SOAPBase64.h
@@ -27,15 +27,68 @@
 #pragma once
 #endif // _MSC_VER > 1000
 
-class EASYSOAP_EXPORT SOAPBase64  
+//
+// These classes are used to wrap arrays
+// so we know how to encode/decode.
+
+//
+// Encode/Decode as base64
+class EASYSOAP_EXPORT SOAPBase64
 {
 public:
+	SOAPBase64(const SOAPArray<char>& carr)
+	{
+		m_carr = &carr;
+		m_arr = 0;
+	}
+
+	SOAPBase64(SOAPArray<char>& arr)
+	{
+		m_arr = &arr;
+		m_carr = 0;
+	}
+
+	~SOAPBase64()
+	{
+	}
+
+	const SOAPArray<char>	*m_carr;
+	SOAPArray<char>			*m_arr;
+
 	static void Encode(const SOAPArray<char>& bytes, SOAPString& str);
 	static void Decode(const SOAPString& str, SOAPArray<char>& bytes);
 private:
 	SOAPBase64();
-	//~SOAPBase64();
+};
 
+//
+// Encode/Decode as hex
+class EASYSOAP_EXPORT SOAPHex
+{
+public:
+	SOAPHex(const SOAPArray<char>& carr)
+	{
+		m_carr = &carr;
+		m_arr = 0;
+	}
+
+	SOAPHex(SOAPArray<char>& arr)
+	{
+		m_arr = &arr;
+		m_carr = 0;
+	}
+
+	~SOAPHex()
+	{
+	}
+
+	const SOAPArray<char>	*m_carr;
+	SOAPArray<char>			*m_arr;
+
+	static void Encode(const SOAPArray<char>& bytes, SOAPString& str);
+	static void Decode(const SOAPString& str, SOAPArray<char>& bytes);
+private:
+	SOAPHex();
 };
 
 #endif // !defined(AFX_SOAPBASE64_H__8831A358_2EA1_11D5_B3F3_000000000000__INCLUDED_)
