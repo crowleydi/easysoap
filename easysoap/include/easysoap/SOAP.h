fef14a21073e8a6da8219a509e9be51ccc8b0882
dcrowley <>
2001-11-28 01:24:36 +0000

    Change version number.

--- easysoap/include/easysoap/SOAP.h
+++ easysoap/include/easysoap/SOAP.h
@@ -25,8 +25,8 @@
 
 #define EASYSOAP_STRING			"EasySoap++"
 #define EASYSOAP_HOMEPAGE		"http://easysoap.sourceforge.net/"
-#define EASYSOAP_VERSION_STRING	"0.5"
-#define EASYSOAP_VERSION		50
+#define EASYSOAP_VERSION_STRING	"0.6"
+#define EASYSOAP_VERSION		60
 
 #include <easysoap/es_conf.h>
 
